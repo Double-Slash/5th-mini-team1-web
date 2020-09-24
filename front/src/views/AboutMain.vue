a716d1c6aced2d5698089632248fd09bd9af09be
jochanmin <happyjarban@gmail.com>
2020-09-24 15:50:30 +0900

    [변경] AboutView -> AboutMain && Appheader 라우터 연결 및 else 오류 제거

---
+++ front/src/views/AboutMain.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="about">
+    <div class='header'>
+      <img src="../assets/img/profile.png" width="320" height="320"/>
+      <div class='title'>사람을 찾습니다!<br>
+      마음이 맞는 프로젝트를 진행하고픈 사람,
+      나를 알리고 싶은 사람,도대체 어디있니?
+      </div>
+      <div class='subtitle'>
+      "어디있니"는 팀을 찾거나 프로젝트를 진행하고 싶을 때 함께 할 팀 혹은 개인을 찾는 기능으로
+      특정 나이대에 국한되지 않고 협업할 사람을 찾을 수 있게 도와주는 웹서비스입니다.
+      </div>
+      <div style='clear:both;'></div>
+    </div>
+
+    어디있니 팀원소개
+    <div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+#about{
+  width:80%;
+  margin: 0px auto;
+  text-align: left;
+}
+.header img{
+  border-radius: 500px;
+  margin-right:30px;
+  float:left;
+}
+.title{
+  padding-top:50px;
+  font-size:30px;
+  font-weight: bold;
+}
+.subtitle{
+  font-size:24px;
+}
+</style>
