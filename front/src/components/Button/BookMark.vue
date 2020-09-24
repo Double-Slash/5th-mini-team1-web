2dfbe1bd11d66cc2277c06364a935addb40ec24f
chamincheol <54410332+chamincheol@users.noreply.github.com>
2020-09-24 15:38:19 +0900

    [추가] : 버튼 컴포넌트 추가(BookMark, HashTag)

---
+++ front/src/components/Button/BookMark.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <img src="@/assets/svg/bookmark.svg" />
+  </div>
+</template>
+
+<script>
+export default {
+  // to do
+  // 저장 버튼을 눌렀을 때 로직을 작성할 예정
+};
+</script>
+
+<style scoped>
+div {
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  width: 48px;
+  height: 48px;
+  border: 0;
+  box-sizing: border-box;
+}
+svg {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ front/src/components/Button/HashTag.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <span>{{ hashTagText }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    hashTagText() {
+      return `#${this.item}`;
+    },
+  },
+};
+</script>
+
+<style scoped>
+div {
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  width: 64px;
+  height: 24px;
+  margin-right: 8px;
+  border-radius: 8px;
+  font-size: 12px;
+  color: #ffffff;
+  background-color: #2e88db;
+}
+</style>
