988baefc61f97a93a5f220c284cf9d1ee96cf6fc
chamincheol <54410332+chamincheol@users.noreply.github.com>
2020-10-07 21:33:35 +0900

    [변경] : submitLogIn action 함수 변경

--- front/src/store/actions.js
+++ front/src/store/actions.js
@@ -6,13 +6,11 @@ import { setTokenCookie } from "@/utils/cookies";
 export default {
   // log in data 전달
   async submitLogIn({ commit }, data) {
-    try {
-      commit("setErrorMessage", "");
-      const { token } = await postLogIn(data);
-      commit("setToken", token);
-      setTokenCookie(token);
-    } catch (error) {
-      commit("setErrorMessage", error.response.data);
-    }
-  },
+    const {
+      data: { token }
+    } = await postLogIn(data);
+    commit("setToken", token);
+    setTokenCookie(token);
+    return token;
+  }
 };
