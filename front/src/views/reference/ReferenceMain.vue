8e9e11d137c928b64cceb3bdefde0b3f4de78553
kmin-jeong <desk535353@gmail.com>
2020-10-08 21:16:44 +0900

    [추가]: referencemain 컴포넌트 작업

--- front/src/views/reference/ReferenceMain.vue
+++ front/src/views/reference/ReferenceMain.vue
@@ -1,5 +1,61 @@
 <template>
-  <div>ReferenceMain</div>
+    <div class="reference_main">
+         <div class="ads_pictures">
+           <img src="@/assets/svg/referencemain.svg" width="267" height="180"/>
+            <div class = "open_ads">
+            오픈 에즈
+            </div>       
+            <div class=" explain">
+             마케팅 정보 큐레이션   
+            </div>  
+        </div>
+        <div class="ads_picture">
+           <img src="@/assets/svg/referencemain.svg" width="267" height="180"/>
+        </div>
+        <div class="ads_picture">
+           <img src="@/assets/svg/referencemain.svg" width="267" height="180"/>
+        </div>
+        <div class="ads_picture">
+           <img src="@/assets/svg/referencemain.svg" width="267" height="180"/>
+        </div>
+        <div class="ads_picture">
+           <img src="@/assets/svg/referencemain.svg" width="267" height="180"/>
+        </div>
+        <div class="ads_picture">
+           <img src="@/assets/svg/referencemain.svg" width="267" height="180"/>
+        </div>
+        <div class="ads_picture">
+           <img src="@/assets/svg/referencemain.svg" width="267" height="180"/>
+        </div>
+        <div class="ads_picture">
+           <img src="@/assets/svg/referencemain.svg" width="267" height="180"/>
+        </div>
+        <div class="ads_picture">
+           <img src="@/assets/svg/referencemain.svg" width="267" height="180"/>
+        </div>
+        <div class="ads_picture">
+           <img src="@/assets/svg/referencemain.svg" width="267" height="180"/>
+        </div>
+        <div class="ads_picture">
+           <img src="@/assets/svg/referencemain.svg" width="267" height="180"/>
+        </div>
+        <div class="ads_picture">
+           <img src="@/assets/svg/referencemain.svg" width="267" height="180"/>
+        </div>
+        <div class="ads_picture">
+           <img src="@/assets/svg/referencemain.svg" width="267" height="180"/>
+        </div>
+        <div class="ads_picture">
+           <img src="@/assets/svg/referencemain.svg" width="267" height="180"/>
+        </div>
+        <div class="ads_picture">
+           <img src="@/assets/svg/referencemain.svg" width="267" height="180"/>
+        </div>
+        <div class="ads_picture">
+           <img src="@/assets/svg/referencemain.svg" width="267" height="180"/>
+        </div>
+    </div>
+
 </template>
 
 <script>
@@ -9,5 +65,35 @@ export default {
 </script>
 
 <style>
+.open_ads{
+    font-size:18px;
+    font-weight: bold;
+}
+.explain{
+    font-size:12px;
+}
+.reference_main{
+    display:grid;
+    grid-template-rows: repeat(4, 180px);
+    grid-template-columns: repeat(4,1fr);
+    row-gap:3.0em;
+    column-gap:1.5em;
+}
+.ads_pictire{
+    display: grid;
+    display:grid;
+    grid-template-rows: repeat(4, 180px);
+    grid-template-columns: repeat(4,1fr);
+    row-gap:3.0em;
+    column-gap:1.5em;
+}
 
+.ads_pictires{
+    display: grid;
+    display:grid;
+    grid-template-rows: repeat(4, 180px);
+    grid-template-columns: repeat(4,1fr);
+    row-gap:3.0em;
+    column-gap:1.5em;
+}
 </style>
