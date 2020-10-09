9edf567cae234c2432c079685cb00cba845e7ba3
chamincheol <54410332+chamincheol@users.noreply.github.com>
2020-10-10 00:29:05 +0900

    [변경] : 로그인 action 변경, 회원가입 action 추가

--- front/src/store/actions.js
+++ front/src/store/actions.js
@@ -1,16 +1,40 @@
-import { postLogIn } from "@/api";
+import { postRegister, postLocalLogIn, postGoogleLogIn } from "@/api";
 
 // cookie
-import { setTokenCookie } from "@/utils/cookies";
+import { setToken } from "@/utils/jwtToken";
 
 export default {
-  // log in data 전달
-  async submitLogIn({ commit }, data) {
-    const {
-      data: { token }
-    } = await postLogIn(data);
-    commit("setToken", token);
-    setTokenCookie(token);
-    return token;
+  // 회원가입
+  async submitRegister({ commit }, registerData) {
+    let response = "";
+    try {
+      const { data } = await postRegister(registerData);
+      // to do...회원가입 결과 값 받고 난 후
+      response = data;
+      return data;
+    } catch (error) {
+      commit("setRegisterError", "존재하지 않은 사용자입니다.");
+      response = "";
+    }
+    return response;
+  },
+  // local login, google login
+  async submitLogIn({ commit }, logInData) {
+    let response = "";
+    try {
+      if (logInData.username) {
+        const { data } = await postLocalLogIn(logInData);
+        response = data;
+      } else {
+        const { data } = await postGoogleLogIn(logInData);
+        response = data;
+      }
+      commit("setToken", response.token);
+      setToken(response.token);
+    } catch (error) {
+      commit("setLogInError", "존재하지 않은 사용자입니다.");
+      response = "";
+    }
+    return response;
   }
 };
--- front/src/store/index.js
+++ front/src/store/index.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import { getTokenCookie } from "@/utils/cookies";
+import { getToken } from "@/utils/jwtToken";
 import getters from "./getters";
 import mutations from "./mutations";
 import actions from "./actions";
@@ -10,7 +10,9 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   strict: process.env.NODE_ENV !== "production",
   state: {
-    token: getTokenCookie() || "" // jwt token
+    token: getToken() || "", // jwt token
+    registerError: "", // 회원가입 서버 에러
+    logInError: "" // log in 서버 에러
   },
   getters,
   mutations,
