93cb220f60a9505b43b296cc634ece50f333b790
chamincheol <54410332+chamincheol@users.noreply.github.com>
2020-10-10 00:32:07 +0900

    [변경] : 스크롤 위치에 따라 헤더 스타일 변경

--- front/src/components/AppHeader.vue
+++ front/src/components/AppHeader.vue
@@ -1,31 +1,38 @@
 <template>
-  <header>
+  <header :style="changeHeader">
     <div class="header-wrapper">
       <router-link to="/" class="logo-wrapper">
         <div class="logo-image"></div>
-        <h1>어디있니</h1>
+        <h1 :style="changeTitle">어디있니</h1>
       </router-link>
       <nav>
-        <router-link to="/partner">파트너 찾기</router-link>
-        <router-link to="/crew">크루 찾기</router-link>
-        <router-link to="/contest">공모전</router-link>
-        <router-link to="/reference">레퍼런스</router-link>
-        <router-link to="/about">ABOUT</router-link>
+        <router-link to="/partner" :style="changeText">파트너 찾기</router-link>
+        <router-link to="/crew" :style="changeText">크루 찾기</router-link>
+        <router-link to="/contest" :style="changeText">공모전</router-link>
+        <router-link to="/reference" :style="changeText">레퍼런스</router-link>
+        <router-link to="/about" :style="changeText">ABOUT</router-link>
       </nav>
       <aside>
-        <ul v-if="token">
-          <li class="image-wrapper">
-            <img src="@/assets/svg/double-bookmark.svg" />
-          </li>
-          <li class="image-wrapper">
-            <img src="@/assets/svg/message.svg" />
-          </li>
-          <!-- to do
-            1.userInfo가 생기면 img에 thumbnail 적용
-           -->
-          <img :src="null" class="image-wrapper user-image" />
+        <ul v-if="token" class="user-login-after">
+          <router-link to="/bookmark">
+            <li class="image-wrapper">
+              <img src="@/assets/svg/double-bookmark.svg" />
+            </li>
+          </router-link>
+          <router-link to="/message">
+            <li class="image-wrapper">
+              <img src="@/assets/svg/message.svg" />
+            </li>
+          </router-link>
+          <router-link to="/profile">
+            <li>
+              <img :src="null" class="image-wrapper user-image" />
+            </li>
+          </router-link>
         </ul>
-        <button v-else @click="clickLogInBtn">Log In</button>
+        <button v-else @click="clickLogInBtn" :style="changeButton">
+          Log In
+        </button>
         <LogInModal v-if="logInModalActive" />
       </aside>
     </div>
@@ -33,22 +40,76 @@
 </template>
 
 <script>
-import LogInModal from "./LogInModal.vue";
 import { mapState } from "vuex";
+import LogInModal from "./LogInModal.vue";
 
 export default {
   components: {
     LogInModal,
   },
+  created() {
+    window.addEventListener("scroll", this.detectWindowScrollY);
+  },
+  mounted() {
+    this.detectWindowScrollY();
+  },
+  destroyed() {
+    window.removeEventListener("scroll", this.detectWindowScrollY);
+  },
   data() {
     return {
+      headerStyle: false, // false : 배경 투명, 글자 흰색. true : 배경 흰색, 글자 검정
       logInModalActive: false,
     };
   },
   computed: {
     ...mapState(["token"]),
+    // 헤더 스타일 변경
+    changeHeader() {
+      if (this.$route.path !== "/") return null;
+      if (this.headerStyle === true) {
+        return {
+          background: "white",
+          boxShadow: "0 3px 20px 0 rgba(0, 0, 0, 0.05) }",
+        };
+      }
+      return { background: "transparent", boxShadow: "0 0 black" };
+    },
+    // 로그인 버튼 스타일 변경
+    changeButton() {
+      if (this.$route.path !== "/") return null;
+      if (this.headerStyle === true) {
+        return {
+          border: "solid 3px #2e88db",
+          color: "#2e88db",
+        };
+      }
+      return {
+        border: "solid 3px white",
+        color: "white",
+      };
+    },
+    // 제목 스타일 변경
+    changeTitle() {
+      if (this.$route.path !== "/") return null;
+      if (this.headerStyle === true) return { color: "#2e88db" };
+      return { color: "white" };
+    },
+    // 메뉴 텍스트 스타일 변경
+    changeText() {
+      if (this.$route.path !== "/") return null;
+      if (this.headerStyle === true) return { color: "black" };
+      return { color: "white" };
+    },
   },
   methods: {
+    detectWindowScrollY() {
+      if (window.scrollY === 0) {
+        this.headerStyle = false;
+      } else {
+        this.headerStyle = true;
+      }
+    },
     clickLogInBtn() {
       this.logInModalActive = true;
     },
@@ -89,6 +150,7 @@ header {
   border-radius: 20px;
   background-color: #2e88db;
 }
+
 /* 라우터 활성화시 */
 .router-link-exact-active {
   color: #2e88db;
@@ -130,9 +192,14 @@ button {
   background: none;
   outline: none;
 }
+
 button:hover {
   background-color: #2e88db;
   color: white;
+}
+
+.user-login-after {
+  display: flex;
 }
 
 .image-wrapper {
@@ -142,6 +209,7 @@ button:hover {
   width: 32px;
   height: 32px;
   margin-left: 24px;
+  cursor: pointer;
 }
 
 .user-image {
