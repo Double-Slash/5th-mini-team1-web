3e15ce64b93fe40b8558434e3866d836cd43f7c3
kmin-jeong <desk535353@gmail.com>
2020-10-11 22:35:16 +0900

    feat: partner write & view, view는 파트너 메세지 창 나오는것 까지 구현함

--- front/src/App.vue
+++ front/src/App.vue
@@ -13,7 +13,6 @@
 <script>
 import AppHeader from "./components/AppHeader.vue";
 import AppFooter from "./components/AppFooter.vue";
-
 export default {
   name: "App",
   components: {
@@ -29,25 +28,21 @@ export default {
   padding-top: 138px;
   margin: 0 auto;
 }
-
 @media (max-width: 1919px) {
   .view-wrapper {
     width: 1376px;
   }
 }
-
 @media (max-width: 1440px) {
   .view-wrapper {
     width: 1280px;
   }
 }
-
 @media (max-width: 1312px) {
   .view-wrapper {
     width: 912px;
   }
 }
-
 /* Router Transition */
 .routing-fade-enter-active,
 .routing-fade-leave-active {
--- front/src/views/MainView.vue
+++ front/src/views/MainView.vue
@@ -1,10 +1,9 @@
 <template>
-  <div>MainView</div>
+  <div>MainView </div>
 </template>
 
 <script>
 export default {
-
 };
 </script>
 
