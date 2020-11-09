19a733c583ebbdf732914f25e30c9b5685ee1603
chamincheol <54410332+chamincheol@users.noreply.github.com>
2020-11-09 22:12:04 +0900

    [삭제] : 기존 Vue 프로젝트 삭제

--- front/src/utils/jwtToken.js
+++
@@ -1,9 +0,0 @@
-function setToken(token) {
-  sessionStorage.setItem("token", token);
-}
-
-function getToken() {
-  return sessionStorage.getItem("token");
-}
-
-export { setToken, getToken };
--- front/src/utils/userId.js
+++
@@ -1,9 +0,0 @@
-function setUserId(id) {
-  sessionStorage.setItem("userId", id);
-}
-
-function getUserId() {
-  return sessionStorage.getItem("userId");
-}
-
-export { setUserId, getUserId };
